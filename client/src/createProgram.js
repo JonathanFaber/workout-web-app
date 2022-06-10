ad66f33d9d4f42fbdd3f82fb155b22e2069b59ca
Jonathan Faber <jonathanfaber1998@gmail.com>
2022-06-10 13:14:56 -0400

    added bootstrap for form and fixed copying

--- client/src/createProgram.js
+++ client/src/createProgram.js
@@ -36,9 +36,10 @@ const CreateProgram = () => {
   const handleAddBlockCopy = (i) => {
     if (program.length < 32) {
       const newProgram = program.slice()
+      const weeks = program[i].weeks
       newProgram.push({ 
         name: `Block ${program.length + 1}`,
-        weeks: program[i].weeks.slice() 
+        weeks: JSON.parse(JSON.stringify(weeks)) 
       })
       setProgram(newProgram)
     }
@@ -63,7 +64,8 @@ const CreateProgram = () => {
   const handleAddWeekCopy = (i, j) => {
     if (program[i].weeks.length < 52) {
       const newProgram = program.slice()
-      newProgram[i].weeks.push({ days: program[i].weeks[j].days.slice() })
+      const days = program[i].weeks[j].days
+      newProgram[i].weeks.push({ days: JSON.parse(JSON.stringify(days)) })
       setProgram(newProgram)
     }
   }
@@ -87,8 +89,9 @@ const CreateProgram = () => {
   const handleAddDayCopy = (i, j, k) => {
     if (program[i].weeks[j].days.length < 32) {
       const newProgram = program.slice()
+      const exercises = program[i].weeks[j].days[k].exercises
       newProgram[i].weeks[j].days.push({ 
-        exercises: program[i].weeks[j].days[k].exercises.slice(), 
+        exercises: JSON.parse(JSON.stringify(exercises)), 
         nextExerciseName: '' })
       setProgram(newProgram)
     }
@@ -164,75 +167,90 @@ const CreateProgram = () => {
   return (
     <form>
       {/*data && <>{data}</>*/}
-      <label htmlFor='program-name'>Program name: </label>
-      <input type='text' name='program-name'></input>
+      <label className='form-label' htmlFor='program-name'>Program name</label>
+      <input type='text' className='form-control form-control-lg' name='program-name'></input>
+
+      <div>
+        <button type='button' className='btn btn-primary' onClick={handleAddBlock}>Add new block</button>
+      </div>
 
       <ul>
-        {program.map((block, i) => (<li key={`block-${i}`}>
-          <div>
-            <input defaultValue={`Block ${i + 1}`} value={block.name} onInput={(e) => handleNameBlock(e, i)}></input>
-            <button type='button' onClick={() => handleAddBlockCopy(i)}>Add copy</button>
+        {program.map((block, i) => <li key={`block-${i}`}>
+          <div class='input-group' role='group'>
+            <input className='form-control' defaultValue={`Block ${i + 1}`} value={block.name} onInput={(e) => handleNameBlock(e, i)}></input>
+            <button type='button' className='btn btn-primary' onClick={() => handleAddBlockCopy(i)}>Add copy</button>
             {program.length > 1 && 
-              <button type='button' onClick={() => handleRemoveBlock(i)}>Remove block</button>}
+              <button type='button' className='btn btn-primary' onClick={() => handleRemoveBlock(i)}>Remove block</button>}
+          </div>
+
+          <div>
+            <button type='button' className='btn btn-primary' onClick={() => handleAddWeek(i)}>Add new week</button>
           </div>
 
           <ul>
-            {block.weeks.map((week, j) => <li key={`week-${i}-${j}`}>
-              <div>
-                <>Week {j + 1}</>
-                <button type='button' onClick={() => handleAddWeekCopy(i, j)}>Add copy</button>
-                <button type='button' onClick={() => handleRemoveWeek(i, j)}>Remove week</button>
+            {block.weeks.map((week, j) => <li key={`week-${i}-${j}`}> 
+              <span className='text'>Week {j + 1} </span>
+
+              <div class='btn-group' role='group'>
+                <button type='button' className='btn btn-primary' onClick={() => handleAddWeekCopy(i, j)}>Add copy</button>
+                {block.weeks.length > 1 && 
+                  <button type='button' className='btn btn-primary' onClick={() => handleRemoveWeek(i, j)}>Remove week</button>}
               </div>
-              <button type='button' onClick={() => handleAddDay(i, j)}>Add new day</button>
+
+              <div>
+                <button type='button' className='btn btn-primary' onClick={() => handleAddDay(i, j)}>Add new day</button>
+              </div>
 
               <ul>
                 {week.days.map((day, k) => <li key={`day-${i}-${j}-${k}`}>
-                  <div>
-                    <>Day {k + 1}</>
-                    <button type='button' onClick={() => handleAddDayCopy(i, j, k)}>Add copy</button>
-                    <button type='button' onClick={() => handleRemoveDay(i, j, k)}>Remove day</button>  
+                  <span className='text'>Day {k + 1} </span>
+
+                  <div class='btn-group' role='group'>
+                    <button type='button' className='btn btn-primary' onClick={() => handleAddDayCopy(i, j, k)}>Add copy</button>
+                    {week.days.length > 1 &&
+                      <button type='button' className='btn btn-primary' onClick={() => handleRemoveDay(i, j, k)}>Remove day</button>}
                   </div>
-                  <div>
-                    <input type='text' value={day.nextExerciseName} onInput={(e) => handleNameNewExercise(e, i, j, k)}></input>
-                    <button type='button' onClick={() => handleAddExercise(i, j, k)}>Add new exercise</button>
+
+                  <div className='input-group'>
+                    <input className='form-control' value={day.nextExerciseName} onInput={(e) => handleNameNewExercise(e, i, j, k)}></input>
+                    <button type='button' className='btn btn-primary' onClick={() => handleAddExercise(i, j, k)}>Add new exercise</button>
                   </div>
 
                   <ul>
                     {day.exercises.map((exercise, l) => <li key={`exercise-${i}-${j}-${k}-${l}`}>
-                      <div>
-                        <input value={exercise.name} onInput={(e) => handleNameExercise(e, i, j, k, l)}></input>
-                        <button type='button' onClick={() => handleRemoveExercise(i, j, k, l)}>Remove exercise</button>
+                      <div className='input-group'>
+                        <input className='form-control' value={exercise.name} onInput={(e) => handleNameExercise(e, i, j, k, l)}></input>
+                        <button type='button' className='btn btn-primary' onClick={() => handleRemoveExercise(i, j, k, l)}>Remove exercise</button>
                       </div>
-                      <div>
-                        <label htmlFor='reps'>Reps:</label>
-                        <input type='number' name='reps' min='0' onChange={(e) => handleNewSetReps(e, i, j, k, l)}></input>
-                        <button type='button' onClick={() => handleAddSet(i, j, k, l)}>Add set</button>
-                        
-                        <ul>
-                          {exercise.sets.map((set, m) => <li key={`set-${i}-${j}-${k}-${l}-${m}`}>
-                            <div>Set {m + 1}: 
-                              <input value={set} onInput={(e) => handleChangeSet(e, i, j, k, l, m)}></input> reps
-                              <button type='button' onClick={() => handleRemoveSet(i, j, k, l, m)}>Remove set</button>
-                            </div>
-                          </li>)}
-                        </ul>
+
+                      <div className='input-group'>
+                        <input type='number' className='form-control' min='0' value={exercise.nextReps} onChange={(e) => handleNewSetReps(e, i, j, k, l)}></input>
+                        <span className='input-group-text'>reps</span>
+                        <button type='button' className='btn btn-primary' onClick={() => handleAddSet(i, j, k, l)}>Add set</button>
                       </div>
+
+                      <ul>
+                        {exercise.sets.map((set, m) => <li key={`set-${i}-${j}-${k}-${l}-${m}`}>
+                          <div className='input-group'>
+                            <span className='input-group-text'>Set {m + 1}: </span>
+                            <input type='number' className='form-control' min='0' value={set} onInput={(e) => handleChangeSet(e, i, j, k, l, m)}></input>
+                            <span className='input-group-text'>reps</span>
+                            <button type='button' className='btn btn-primary' onClick={() => handleRemoveSet(i, j, k, l, m)}>Remove set</button>
+                          </div>
+                        </li>)}
+                      </ul>
                     </li>)}
                   </ul>
                 </li>)}
               </ul>
             </li>)}
           </ul>
-          
-          <button type='button' onClick={() => handleAddWeek(i)}>Add new week</button>
-        </li>))}
+        </li>)}
       </ul>
 
       <div>
-        <button type='button' onClick={handleAddBlock}>Add new block</button>
+        <button type="submit" className='btn btn-primary'>Save Program</button>
       </div>
-
-      <input type="submit" value="Submit"></input>
     </form>
   )
 }

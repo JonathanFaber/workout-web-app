6da567cd2b30fde588cd3503ebc30777e7d324bd
Jonathan Faber <jonathanfaber1998@gmail.com>
2022-07-16 15:29:17 -0400

    Added units

--- client/src/createProgram.js
+++ client/src/createProgram.js
@@ -6,15 +6,27 @@ const CreateProgram = () => {
     name: 'Block 1', 
     weeks: [{ days: [{ exercises: [], nextExerciseName: '' }] }] 
   }])
+
+  const [units, setUnits] = useState('kg')
+  const [increment, setIncrement] = useState(5)
   
   /* Get Data from backend
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("/api")
+    fetch('/api')
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);*/
+
+  const handleUnits = (e) => {
+    setUnits(e.target.value)
+  }
+
+  const handleIncrement = (e) => {
+    if (e.target.value > 0)
+      setIncrement(e.target.value)
+  }
 
   const handleNameBlock = (e, i) => {
     const newProgram = program.slice()
@@ -121,7 +133,7 @@ const CreateProgram = () => {
     if (program[i].weeks[j].days[k].exercises.length < 32) {
       const name = program[i].weeks[j].days[k].nextExerciseName
       const newProgram = program.slice()
-      newProgram[i].weeks[j].days[k].exercises.push({ name, sets: [], nextReps: 1 })
+      newProgram[i].weeks[j].days[k].exercises.push({ name, sets: [], nextReps: 1, nextWeight: 0 })
       newProgram[i].weeks[j].days[k].nextExerciseName = ''
       setProgram(newProgram)
     }
@@ -135,24 +147,37 @@ const CreateProgram = () => {
     }
   }
 
-  const handleNewSetReps = (e, i, j, k, l) => {
+  const handleNewReps = (e, i, j, k, l) => {
     const newProgram = program.slice()
     newProgram[i].weeks[j].days[k].exercises[l].nextReps = e.target.value
+    setProgram(newProgram)
+  }
+
+  const handleNewWeight = (e, i, j, k, l) => {
+    const newProgram = program.slice()
+    newProgram[i].weeks[j].days[k].exercises[l].nextWeight = e.target.value
     setProgram(newProgram)
   }
 
   const handleAddSet = (i, j, k, l) => {
     if (program[i].weeks[j].days[k].exercises[l].sets.length < 32) {
       const reps = program[i].weeks[j].days[k].exercises[l].nextReps
+      const weight = program[i].weeks[j].days[k].exercises[l].nextWeight
       const newProgram = program.slice()
-      newProgram[i].weeks[j].days[k].exercises[l].sets.push(reps)
+      newProgram[i].weeks[j].days[k].exercises[l].sets.push({ reps, weight })
       setProgram(newProgram)
     }
   }
 
-  const handleChangeSet = (e, i, j, k, l, m) => {
+  const handleChangeReps = (e, i, j, k, l, m) => {
     const newProgram = program.slice()
-    newProgram[i].weeks[j].days[k].exercises[l].sets[m] = e.target.value
+    newProgram[i].weeks[j].days[k].exercises[l].sets[m].reps = e.target.value
+    setProgram(newProgram)
+  }
+
+  const handleChangeWeight = (e, i, j, k, l, m) => {
+    const newProgram = program.slice()
+    newProgram[i].weeks[j].days[k].exercises[l].sets[m].weight = e.target.value
     setProgram(newProgram)
   }
 
@@ -172,6 +197,22 @@ const CreateProgram = () => {
         <input type='text' id='program-name' className='form-control form-control-lg' name='program-name'></input>
       </div>
 
+      <span>Units:&nbsp;</span>
+      <div className='form-check form-check-inline'>
+        <input type='radio' className='form-check-input' id='kilos' name='units' value='kg' defaultChecked='checked' onChange={handleUnits}></input>
+        <label className='form-check-label' htmlFor='kilos'>kg</label>
+      </div>
+
+      <div className='form-check form-check-inline'>
+        <input type='radio' className='form-check-input' id='pounds' name='units' value='lbs' onChange={handleUnits}></input>
+        <label className='form-check-label' htmlFor='pounds'>lbs</label>
+      </div>
+
+      <div>
+        <label htmlFor='increment' className='form-label'>Weight increment</label>
+        <input type='number' className='form-control' id='increment' min='1' value={increment} onInput={handleIncrement}></input>
+      </div>
+      
       <div className='block'>
         <button type='button' className='btn btn-outline-primary' onClick={handleAddBlock}>Add new block</button>
       </div>
@@ -225,7 +266,7 @@ const CreateProgram = () => {
                   </div>
 
                   {day.exercises.length > 0 && <ul>
-                    {day.exercises.map((exercise, l) => <li key={`exercise-${i}-${j}-${k}-${l}`} className='border'>
+                    {day.exercises.map((exercise, l) => <li key={`exercise-${i}-${j}-${k}-${l}`}>
                       <hr className='hr-4'/>
                       
                       <div className='input-group'>
@@ -234,8 +275,10 @@ const CreateProgram = () => {
                       </div>
 
                       <div className='input-group'>
-                        <input type='number' className='form-control' min='0' value={exercise.nextReps} onChange={(e) => handleNewSetReps(e, i, j, k, l)}></input>
-                        <span className='input-group-text'>reps</span>
+                        <input type='number' className='form-control' min='0' value={exercise.nextReps} onChange={(e) => handleNewReps(e, i, j, k, l)}></input>
+                        <span className='input-group-text'>reps at</span>
+                        <input type='number' className='form-control' step={increment} value={exercise.nextWeight} onInput={(e) => handleNewWeight(e, i, j, k, l)}></input>
+                        <span className='input-group-text'>{units}</span>
                         <button type='button' className='btn btn-outline-primary' onClick={() => handleAddSet(i, j, k, l)}>Add set</button>
                       </div>
 
@@ -243,8 +286,10 @@ const CreateProgram = () => {
                         {exercise.sets.map((set, m) => <li key={`set-${i}-${j}-${k}-${l}-${m}`}>                          
                           <div className='input-group'>
                             <span className='input-group-text'>Set {m + 1}:</span>
-                            <input type='number' className='form-control' min='0' value={set} onInput={(e) => handleChangeSet(e, i, j, k, l, m)}></input>
-                            <span className='input-group-text'>reps</span>
+                            <input type='number' className='form-control' min='0' value={set.reps} onInput={(e) => handleChangeReps(e, i, j, k, l, m)}></input>
+                            <span className='input-group-text'>reps at</span>
+                            <input type='number' className='form-control' step={increment} value={set.weight} onInput={(e) => handleChangeWeight(e, i, j, k, l, m)}></input>
+                            <span className='input-group-text'>{units}</span>
                             <button type='button' className='btn btn-outline-danger' onClick={() => handleRemoveSet(i, j, k, l, m)}>Remove set</button>
                           </div>
                         </li>)}
@@ -259,7 +304,7 @@ const CreateProgram = () => {
       </ul>
 
       <div className='group'>
-        <button type="submit" className='btn btn-primary'>Save Program</button>
+        <button type='submit' className='btn btn-primary'>Save Program</button>
       </div>
     </form>
   )
